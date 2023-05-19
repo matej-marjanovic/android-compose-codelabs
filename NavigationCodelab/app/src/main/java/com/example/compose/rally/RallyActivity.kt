97ef71502ad43f1de6c501c6b744d7b25f5ce30c
Matej Marjanovic <matej.marjanovich@gmail.com>
2023-05-19 10:39:44 +0200

    5. Integrate RallyTabRow with navigation

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -28,8 +28,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.overview.OverviewScreen
@@ -47,17 +50,36 @@ class RallyActivity : ComponentActivity() {
         }
     }
 }
+fun NavHostController.navigateSingleTopTo(route: String) =
+    this.navigate(route) {
+        popUpTo(
+            this@navigateSingleTopTo.graph.findStartDestination().id
+        ) {
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
 
 @Composable
 fun RallyApp() {
     RallyTheme {
-        var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
-        var navController = rememberNavController()
+        val navController = rememberNavController()
+        val currentBackStack by navController.currentBackStackEntryAsState()
+        val currentDestination = currentBackStack?.destination
+
+        val currentScreen: RallyDestination = rallyTabRowScreens.find { rallyDestination ->
+            rallyDestination.route == (currentDestination?.route ?: "No route")
+        } ?: Overview
+
+
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = rallyTabRowScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { screen ->
+                        navController.navigateSingleTopTo(screen.route)
+                                    },
                     currentScreen = currentScreen
                 )
             }
@@ -80,3 +102,4 @@ fun RallyApp() {
         }
     }
 }
+
